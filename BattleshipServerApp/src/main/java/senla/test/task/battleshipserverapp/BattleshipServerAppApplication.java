67b97d20c76ba35242051b128b935415cf280d3c
IcheVell <ichivell@mail.ru>
2026-06-09 22:37:04 +0700

    init commit

---
+++ BattleshipServerApp/src/main/java/senla/test/task/battleshipserverapp/BattleshipServerAppApplication.java
@@ -0,0 +1,13 @@
+package senla.test.task.battleshipserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleshipServerAppApplication.class, args);
+    }
+
+}
---
+++ BattleshipServerApp/src/test/java/senla/test/task/battleshipserverapp/BattleshipServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package senla.test.task.battleshipserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
